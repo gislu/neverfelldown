aef922b3799a60bc06012bc76243a2ec606757a5
陆小凤 <ahlxt@qq.com>
2016-11-29 05:18:38 -0500

    second

--- second apps/own explorer/own explorer/FirstViewController.swift
+++ second apps/own explorer/own explorer/FirstViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var urlInput: UITextField!
+    @IBOutlet weak var urlButton: UIButton!
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+            }
 
+    
+    @IBAction func btnclick(_ sender: AnyObject) {
+        let url = URL(string: urlInput.text!)
+        let alert = UIAlertController(title: nil, message: urlInput.text!, preferredStyle: .alert)
+        let action = UIAlertAction(title: "sure", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        webView.loadRequest(URLRequest(url: url!))
+
+    }
 
 }
 
